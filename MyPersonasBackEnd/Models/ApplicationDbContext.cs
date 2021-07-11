59715da01d1552ee193b4627b09f9011980e24d1
Choki12 <tumanechoki@gmail.com>
2021-07-11 19:21:58 +0200

    Added Testee Class, DB Context and modified Startup.cs to add Sqlite. Added ECF Tools.

---
+++ MyPersonasBackEnd/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyPersonasBackEnd.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Testee> Testees { get; set;}
+
+    }
+}
---
+++ MyPersonasBackEnd/Models/Testee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPersonasBackEnd.Models
+{
+    /*Class representing the test taker*/
+    public class Testee
+    {
+        [Key] //Object Relational Mapping in Entity Framework infers by the attribute name Id that this will be the PK in Testee
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public String Name { get; set; }
+
+        [StringLength(200)]
+        public String Surname { get; set; }
+
+        [StringLength(400)]
+        public String Email { get; set; }
+
+        [StringLength(400)]
+        public String DOB { get; set; }
+
+        [StringLength(400)]
+        public String UserName { get; set; }
+
+
+
+
+
+
+    }
+}
