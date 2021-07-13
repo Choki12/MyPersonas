a2b9e22253bc2aa4ee321c0a33ec768219613feb
Choki12 <tumanechoki@gmail.com>
2021-07-14 01:13:48 +0200

    Added EF migrations and Personality Profiler DB. Crreated API Controller for testees.

---
+++ MyPersonasBackEnd/Controllers/TesteesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyPersonasBackEnd.Models;
+
+namespace MyPersonasBackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TesteesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TesteesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Testees
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Testee>>> GetTestees()
+        {
+            return await _context.Testees.ToListAsync();
+        }
+
+        // GET: api/Testees/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Testee>> GetTestee(int id)
+        {
+            var testee = await _context.Testees.FindAsync(id);
+
+            if (testee == null)
+            {
+                return NotFound();
+            }
+
+            return testee;
+        }
+
+        // PUT: api/Testees/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTestee(int id, Testee testee)
+        {
+            if (id != testee.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(testee).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TesteeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Testees
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Testee>> PostTestee(Testee testee)
+        {
+            _context.Testees.Add(testee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTestee", new { id = testee.Id }, testee);
+        }
+
+        // DELETE: api/Testees/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Testee>> DeleteTestee(int id)
+        {
+            var testee = await _context.Testees.FindAsync(id);
+            if (testee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Testees.Remove(testee);
+            await _context.SaveChangesAsync();
+
+            return testee;
+        }
+
+        private bool TesteeExists(int id)
+        {
+            return _context.Testees.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ MyPersonasBackEnd/Migrations/20210711173536_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyPersonasBackEnd.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Testees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Surname = table.Column<string>(type: "TEXT", maxLength: 200, nullable: true),
+                    Email = table.Column<string>(type: "TEXT", maxLength: 400, nullable: true),
+                    DOB = table.Column<string>(type: "TEXT", maxLength: 400, nullable: true),
+                    UserName = table.Column<string>(type: "TEXT", maxLength: 400, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Testees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Testees");
+        }
+    }
+}
